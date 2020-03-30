0c65a3445070a31702af42aa38771ec7d157caaf
Brandon Tripp <brandongtripp@gmail.com>
2020-03-30 16:29:04 -0600

    Updates

---
+++ precache-manifest.91a43b61ec2f084f0dfc16dc050e212d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ce05e7f1dbb30d21c5fd0b91eda2f0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b14380346aa2f50c95b1",
+    "url": "/monsters-rolodex/static/css/main.7284e742.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14380346aa2f50c95b1",
+    "url": "/monsters-rolodex/static/js/main.df70a3c3.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
